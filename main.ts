0b7941acb6e05675714bebfb67459de4d1bc4219
agnotario <agnotario@gmail.com>
2024-10-13 12:24:58 +0200

    add namespace

--- main.ts
+++ main.ts
@@ -1,1 +1,18 @@
 
+
+//% color=#0fbc11 icon="\uf205"
+//% block="LED Marquesine"
+namespace ledMarquesine {
+
+    //% block="Set GPIO %pin to %value"
+    //% pin.min=0 pin.max=40
+    export function setGpioPin(pin: number, value: number): void {
+        setPin(pin, value);  // Llama a la función C++
+    }
+
+    // Shim para vincular TypeScript con la función C++
+    //% shim=setPin
+    function setPin(pin: number, value: number): void {
+        // Este cuerpo vacío será sustituido por la implementación en C++
+    }
+}
